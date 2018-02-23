8b61368d21f5a4d4866aa8926a9f1d2fca0b561c
rasikag <rasikagayan90@gmail.com>
2018-02-24 07:15:29 +0800

    splash creen

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import SplashScreen from 'react-native-splash-screen'
 
 export default class App extends Component {
 
-  render(){
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+  
+  render() {
     return (
       <View>
         <Text>Hello</Text>
